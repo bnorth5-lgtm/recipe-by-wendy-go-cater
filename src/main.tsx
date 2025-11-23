efe7caf23879fa85459ab2182c3be75db0cac9c6
[dyad] <bnorth5@gmail.com>
2025-11-22 19:38:02 -0500

    [dyad] Removed React Router from App.tsx and added console logs to debug persistent blank screen - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
+console.log("main.tsx executing"); // Debugging log
+
 createRoot(document.getElementById("root")!).render(<App />);
